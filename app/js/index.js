519de013696fed9d5624b3be4a9841e279f2f775
Marani Matias Ezequiel <maranimatias@gmail.com>
2016-06-18 16:32:00 -0300

    ui css table

--- app/js/index.js
+++ app/js/index.js
@@ -15,6 +15,7 @@ window.addEventListener('contextmenu',  (e) => {
 // no se porque cuando la pantalla es menor a 770 tiene que se distinta la posicion
 // con angular usar $scope.$apply(function(){}); para aplicar los cambios 
 window.onresize = function(event) {
+  // algo esta mal  ¿css?
   let size = window.outerHeight < 770 ? 81 : 65;
   $('#statusbar').css( 'top', window.outerHeight - size);
 };
